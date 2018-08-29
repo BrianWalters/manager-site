d0133e0d33475ec611b80a4b5baedcb936ab44db
Brian Walters <brianw@pixotech.com>
2018-08-29 17:44:07 -0500

    cheeseburger

---
+++ cheeseburger.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.js-cheeseburger-burger');
+
+    if (button) {
+        button.addEventListener('click', function(event) {
+
+        });
+    }
+});
