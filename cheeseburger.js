d9e2bbb47197fcffded39af858ac70dc8dbb57a6
Brian Walters <brianw@pixotech.com>
2018-08-29 17:50:03 -0500

    finish cheeeeeseburger easter egg

--- cheeseburger.js
+++ cheeseburger.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('.js-cheeseburger-burger');
+    const button = document.querySelector('.js-cheeseburger-button');
 
     if (button) {
         button.addEventListener('click', function(event) {
-
+            document
+                .querySelector('.apply__tyler-cheeseburger')
+                .classList.add('apply__tyler-cheeseburger--showing');
         });
     }
 });
